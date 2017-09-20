b99f6ef4281c8cb8be815108511b0f305188f80e
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-09-20 16:55:44 -0700

    finish personal site first iteration

--- assets/js/customization.js
+++ assets/js/customization.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   let emailLink = document.getElementById("email-link");
   let sendMessage = document.getElementById("send-message");
-  
+
   emailLink.addEventListener("click", (e) => {
     e.preventDefault();
-    
-    window.location.href = "mailto:ar2au@virginia.edu";
+
+    window.location.href = "mailto:ryan.murphy@alumni.stanford.edu";
   });
-  
+
   sendMessage.addEventListener("click", (e) => {
     e.preventDefault();
-    
+
     let subject = document.getElementById("name").value;
     let body = document.getElementById("message").value;
-    
-    window.location.href = `mailto:ar2au@virginia.edu?subject=${subject}&body=${body}`;
+
+    window.location.href = `mailto:ryan.murphy@alumni.stanford.edu?subject=${subject}&body=${body}`;
   });
 });
